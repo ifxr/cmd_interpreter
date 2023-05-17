d830571cb08c15a075d95428ed3d1c99a9f09511
Luis Arguiles <lmarquez8301@sdsu.edu>
2023-05-17 11:32:18 -0700

    first commit - Added main function

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+*
+*/
+#include "getword.h"
+
+/*
+* Main function serves to test whether "getword" is returning the correct values.
+* Ex: s = "Null&void" c = 4, 1, 4
+* Ex: s = "Null/&void" c = 9
+*/
+int main(){
+	int c;
+	char s[STORAGE];
+	
+	for(;;){
+		(void) printf("n=%d, s=[%s]\n", c = getword(s), s);
+		if (c == -1) break;
+	}
+}
